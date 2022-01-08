b9bbf73aba2e064fd0aa022ae445fdd02fb42b87
victormagalhaes3605 <victor.magalhaes3605@gmail.com>
2022-01-08 17:03:59 -0300

    criei o ambiente e configurei o git

---
+++ senac/src/main/java/projeto/senac/modelo/Usuario.java
@@ -0,0 +1,35 @@
+package projeto.senac.modelo;
+
+import java.util.Date;
+
+public class Usuario {
+	private int id;
+	private String email;
+	private String nome;
+	private Date nascimento;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+}
